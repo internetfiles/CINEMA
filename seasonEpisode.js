23452cb17355dc4587b114a5ac6721dce9ac8f71
Jacksparrowpro <70373238+internetfiles@users.noreply.github.com>
2024-02-24 01:29:04 +0300

    Update seasonEpisode.js

--- seasonEpisode.js
+++ seasonEpisode.js
@@ -132,9 +132,9 @@ const CurrEpisode = async (tv_id, currSea, currEpiso) => {
 };
 
 const releasedEpisode = function (curre) {
-  return `<a class="released_link" href="https://www.2embed.cc/embedtv/${fetcid}&s=${
+  return `<a class="unreleased_link" href=" https://www.2embed.cc/embedtv?id=${fetcid}&s=${
     curre.season_number
-  }&e=${curre.episode_number}"><img class="episode_image"
+  }&e=${curre.episode_number}"><img class="episode_image unreleased_image"
     src="./resources/D moviesand tv show.png" alt="${curre.name}">
     <span class="runtime">${timeCon(curre.runtime)}</span>
 <div class="playsvg_container"><img class="playsvg" src="./resources/play-circle-fill.svg"
@@ -149,7 +149,7 @@ const releasedEpisode = function (curre) {
 };
 
 const unreleasedEpisodev = function (curre) {
-  return `<div><img class="episode_image unreleased_image"
+  return `<div><img class="episode_image"
     src="https://image.tmdb.org/t/p/w500/${curre.still_path}" alt="${
     curre.name
   }">
