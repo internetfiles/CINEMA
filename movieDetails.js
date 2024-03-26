f75691deee0a0db657dc085cd818412fc9697651
Jacksparrowpro <70373238+internetfiles@users.noreply.github.com>
2024-03-26 13:17:01 +0300

    Update movieDetails.js

--- movieDetails.js
+++ movieDetails.js
@@ -303,16 +303,7 @@ const html2 = function (moviee) {
                             d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
                     </svg></span></p>
         </div>
-        <div class="playButtonContainer"> 
-            <span > <a class="GoogleButton" href="https://www.google.com/search?q=${
-              moviee.title +
-              " " +
-              "(" +
-              dateFormatter(moviee.release_date) +
-              ")"
-            }" target="_blank">Google It!</a> </span> 
-            </div>
-            
+                
             <div class="playButtonContainer"> 
             <span > <a class="GoogleButton" href="https://damagecontributionexcessive.com/ku0a4f9m8?key=b9efe2a691b31ec00526f1c2d77da2e2" >Fast Download</a> </span> 
             </div>
